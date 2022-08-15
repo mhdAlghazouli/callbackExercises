d1bd3ceb4ad160bd501b7768da1d5277412c0f35
mhd1995 <35552987+mhd1995@users.noreply.github.com>
2022-08-15 17:57:40 -0400

    Added one more exercise to script.js

--- script.js
+++ script.js
@@ -39,3 +39,25 @@
 // }
 
 // console.log(forEach(letters, alphabet)) //should output abcd
+
+
+
+
+// Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
+
+
+// function intersection (...arrays) {
+//   return arrays.reduce((ele,occu) => {
+//     return occu.filter ((el) => {
+//       if(ele.includes(el)){
+//         return el
+//       }
+//     });
+//   });
+// }
+
+
+
+// console.log(
+//   intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
+// );//Should output [5, 15]
